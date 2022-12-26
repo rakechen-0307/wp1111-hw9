348906bdac45367d1a0531ac8df01b0819ed0f26
rakec <rakechen168@gmail.com>
2022-12-26 15:44:36 +0800

    hw9

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,7 +27,7 @@ app.use(cors());
 app.use('/', routes);
 
 // define server
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 6934;
 
 app.listen(port, () =>
     console.log(`Example app listening on port ${port}!`),
