2f134aa773e95794caefa727f6641977dffbcb19
rakec <rakechen168@gmail.com>
2022-12-26 16:58:29 +0800

    hw9

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const API_ROOT =
-  process.env.NODE_ENV === "production"
-    ? "/api"
-    : "http://localhost:4000/api";
+const API_ROOT = "http://localhost:4000/api";
 
 console.log(API_ROOT)
 
