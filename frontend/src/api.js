c4845f30a47a89e2009f9859ef78b94e297f2d60
rakec <rakechen168@gmail.com>
2022-12-26 16:01:40 +0800

    hw9

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,4 +5,6 @@ const API_ROOT =
     ? "/api"
     : "http://localhost:4000/api";
 
-export const api = axios.create({ baseURL: API_ROOT });
+const api = axios.create({ baseURL: API_ROOT });
+
+export default api
